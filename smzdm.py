9faf90d936488a42c57f70798a2d9e2e465996cc
Benz1 <o3325qq@gmail.com>
2020-10-28 12:21:54 +0800

    update smzdm

--- smzdm.py
+++ smzdm.py
@@ -1,59 +1,35 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- coding: utf8 -*-
+import requests, json, time, os
 
-import os
-import json
-import sys
-import requests
-import re
+requests.packages.urllib3.disable_warnings()
 
-SMZDM_COOKIES = os.environ["SMZDM_COOKIES"]
+cookie = os.environ.get("SMZDM_COOKIES")
 
-class SMZDMDailyException(Exception):
-    def __init__(self, req):
-        self.req = req
+def smzdm_pc(*arg):
+    SCKEY = os.environ.get('SECRETKEY')
+    s = requests.Session()
+    s.headers.update({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'})
+    t = round(int(time.time() * 1000))
+    url = f'https://zhiyou.smzdm.com/user/checkin/jsonp_checkin?_={t}'
 
-    def __str__(self):
-        return str(self.req)
+    headers = {
+        "cookie" : cookie,
+        'Referer': 'https://www.smzdm.com/'
+        }
 
-class SMZDMDaily(object):
-    BASE_URL = 'https://zhiyou.smzdm.com'
-    CHECKIN_URL = BASE_URL + '/user/checkin/jsonp_checkin'
+    r = s.get(url, headers=headers, verify=False)
+    print(r.text)
+    if r.json()["error_code"] != 0 and SCKEY:
+        scurl = f"https://sc.ftqq.com/{SCKEY}.send"
+        data = {
+                "text" : "smzdm  Cookie过期",
+                "desp" : r.text
+                }
+        requests.post(scurl, data=data)
+        print("smzdm cookie失效")
 
-    def __init__(self):
-        self.session = requests.Session()
+if __name__ == "__main__":
+    if cookie:
+        smzdm_pc()
 
-    def checkin(self):
-        headers = {
-            'Accept': '*/*',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-            'Connection': 'keep-alive',
-            'Host': 'zhiyou.smzdm.com',
-            'Referer': 'https://www.smzdm.com/',
-            'Sec-Fetch-Dest': 'script',
-            'Sec-Fetch-Mode': 'no-cors',
-            'Sec-Fetch-Site': 'same-site',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
-            }
-        self.session.headers = headers
-        headers['Cookie'] = SMZDM_COOKIES  
-        r = self.session.get(self.CHECKIN_URL)
-        if r.status_code != 200:
-            raise SMZDMDailyException(r)
-
-        data = r.text
-        jdata = json.loads(data)
-
-        return jdata
-
-if __name__ == '__main__':
-    try:
-        smzdm = SMZDMDaily()
-        result = smzdm.checkin()
-    except SMZDMDailyException as e:
-        print('fail', e)
-    except Exception as e:
-        print('fail', e)
-    else:
-        print('success', result)
+    
