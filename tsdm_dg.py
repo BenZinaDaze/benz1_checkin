f8fb8936a8931d98fcc39ad9d36be6554568047a
BenZinaDaze <o3325qq@gmail.com>
2023-04-21 16:42:02 +0800

    Update tsdm_dg.py

--- tsdm_dg.py
+++ tsdm_dg.py
@@ -2,7 +2,7 @@ import requests
 import os
 
 cookie = os.environ["COOKIETS"]
-url = "https://www.tsdm39.net/plugin.php?id=np_cliworkdz:work"
+url = "https://www.tsdm39.com/plugin.php?id=np_cliworkdz:work"
 data1 = {"act": "clickad"}
 data2 = {"act": "getcre"}
 
@@ -12,7 +12,7 @@ def q():
         "user-agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
         "cookie": cookie,
-        "Referer": "https://www.tsdm39.net/plugin.php?id=np_cliworkdz:work",
+        "Referer": "https://www.tsdm39.com/plugin.php?id=np_cliworkdz:work",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "zh-CN,zh;q=0.9",
         "Connection": "keep-alive",
